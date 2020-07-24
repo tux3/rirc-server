20e33fabeb8b47e89e8ace1c62fc246a9bbf1ad9
tux3 <barrdetwix@gmail.com>
2020-07-24 18:04:56 +0200

    Add basic server example

---
+++ examples/basic_server.rs
@@ -0,0 +1,12 @@
+use rirc_server::{Server, ServerSettings, ServerCallbacks};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let mut server = Server::new(ServerSettings {
+        listen_addr: "0.0.0.0:6667".parse().unwrap(),
+        server_name: "example-server".to_owned(),
+        ..Default::default()
+    }, ServerCallbacks::default());
+
+    server.start().await
+}
